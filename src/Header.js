58e20d8eb9577238aaa49b78121b1f5842018aa9
Yumin Chu <yuminc03@gmail.com>
2024-03-25 11:35:09 +0900

    Feat: Header.js - stylesheet 적용

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,24 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
-export default () => {
   return (
     <View>
       <Text>친구</Text>
+
+export default () => {
+  return (
+    <View style={styles.headerContainer}>
+      <Text style={styles.title}>친구</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+
+  },
+})
