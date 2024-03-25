c1b9d0209ee7175cf94246b80312914619e38dda
Yumin Chu <yuminc03@gmail.com>
2024-03-25 11:34:15 +0900

    Feat: react-native-iphone-x-helper 라이브러리 사용

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Header from './src/Header';
+import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
+
+const statusBarHeight = getStatusBarHeight(true);
+const bottomSpace = getBottomSpace();
+console.log(`${statusBarHeight}, ${bottomSpace}`);
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <Header/>
-    </SafeAreaView>
+    </View>
   );
 }
 
@@ -14,7 +19,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: statusBarHeight,
   },
 });
