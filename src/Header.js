7c6c387b7e7f3bdc2b5e1de4210d0122c21ac05e
Yumin Chu <yuminc03@gmail.com>
2024-03-26 16:38:34 +0900

    Feat: 친구 리스트 Header UI

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,28 @@
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text } from "react-native"
+import { Ionicons } from '@expo/vector-icons';
 
+const IconButton = (props) => {
   return (
-    <View>
-      <Text>친구</Text>
+    <View style={{ paddingHorizontal: 6 }}>
+      <Ionicons name={props.name} size={24} color="black" />
+    </View>
+  );
+}
 
 export default () => {
   return (
-    <View style={styles.headerContainer}>
-      <Text style={styles.title}>친구</Text>
+    <View style={{ 
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingVertical: 10,
+    }}>
+      <Text style={{ fontSize: 22, fontWeight: 'bold' }}>친구</Text>
+      <View style={{ flexDirection: 'row' }}>
+        <IconButton name="search-outline"/>
+        <IconButton name="person-add-outline"/>
+        <IconButton name="musical-notes-outline"/>
+        <IconButton name="settings-outline"/>
+      </View>
     </View>
-  )
+  );
 }
-
-const styles = StyleSheet.create({
-  headerContainer: {
-    flexDirection: 'row',
-  },
-  title: {
-    fontSize: 22,
-    fontWeight: 'bold',
-
-  },
-})
