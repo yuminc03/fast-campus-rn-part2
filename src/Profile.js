007845c1f2d68ffb6d5c472d3f66b467577b869b
Yumin Chu <yuminc03@gmail.com>
2024-03-28 10:51:37 +0900

    Refactor: styled-components 설치 후 적용

--- src/Profile.js
+++ src/Profile.js
@@ -1,23 +1,42 @@
-import { Image, View, Text } from "react-native";
+import { View } from "react-native";
 import Margin from "./Margin";
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex-direction: row;
+`;
+const ProfileImage = styled.Image`
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
+  borderRadius: ${(props) => props.size * 0.4}px;
+`;
+const TextContainer = styled.View`
+  justfy-content: center;
+  margin-left: 10px;
+`;
+const NameText = styled.Text`
+  font-weight: ${(props) => props.isMe ? "bold" : "normal"};
+  font-size: ${(props) => props.isMe ? 16 : 15}px;
+`;
+const IntroductionText = styled.Text`
+  font-size: ${(props) => props.isMe ? 12 : 11 }px;
+  color: gray;
+`;
 
 export default ({ uri, name, introduction, isMe }) => {
   const size = isMe ? 50 : 40;
   return (
-    <View style={{ flexDirection: 'row' }}>
-      <Image 
-        source={{ uri }} 
-        style={{ width: size, height: size, borderRadius: size * 0.4 }}
-      />
-      <View style={{ justifyContent: 'center', marginLeft: 10 }}>
-        <Text style={{ fontWeight: isMe ? 'bold' : undefined, fontSize: isMe ? 16 : 15 }}>{name}</Text>
+    <Container>
+      <ProfileImage source={{ uri }} size={size}/>
+      <TextContainer>
+        <NameText isMe={isMe}>{name}</NameText>
         {!!introduction && (
           <View>
-            <Margin style={{ height: isMe ? 6 : 2 }}/>
-            <Text style={{ fontSize: isMe ? 12 : 11, color: 'gray' }}>{introduction}</Text>
+            <Margin height={ isMe ? 6 : 2 }/>
+            <IntroductionText isMe={isMe}>{introduction}</IntroductionText>
           </View>
         )}
-      </View>
-    </View>
+      </TextContainer>
+    </Container>
   );
 };
