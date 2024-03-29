61652f86370d4f9264df994118d983da04e3cc43
Yumin Chu <yuminc03@gmail.com>
2024-03-29 16:29:47 +0900

    Feat: 계산기 UI, styled-component를 사용하여 공통 style 표현

--- calulator/App.js
+++ calulator/App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import Calculator from './src/Calculator';
 
 export default function App() {
+  const [input, setInput] = useState(0);
+  const [currentOperator, setCurrentOperator] = useState(null);
+  const [result, setResult] = useState(null);
+  const [tempInput, setTempInput] = useState(null);
+  const [tempOperator, setTempOperator] = useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Calculator/>
+    </SafeAreaView>
   );
 }
 
