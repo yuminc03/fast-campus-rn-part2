1cac0c1f294f7186696ad1595409ec24dfd687f3
Yumin Chu <yuminc03@gmail.com>
2024-04-04 10:04:35 +0900

    Feat: FlatList를 사용해서 달력 UI 만들기

--- todo-calendar/App.js
+++ todo-calendar/App.js
@@ -1,18 +1,101 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
 import { runPracticeDayjs } from './src/practice-dayjs';
 import { useEffect } from 'react';
+import { SimpleLineIcons } from '@expo/vector-icons';
+import { getCalendarColumns, getDayColor, getDayText } from './src/util';
+import Margin from './src/Margin';
+import dayjs from 'dayjs';
+
+const columnSize = 35;
+const Column = ({
+  text,
+  color,
+  opacity,
+}) => {
+  return (
+    <View 
+      style={{ 
+        width: columnSize, 
+        height: columnSize,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text
+        style={{
+          color: color,
+          opacity: opacity,
+        }}
+      >
+        {text}
+      </Text>
+    </View>
+  );
+};
+const ArrowButton = ({ iconName, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={{ paddingHorizontal: 20, paddingVertical: 15 }}>
+      <SimpleLineIcons name={iconName} size={15} color="#404040" />
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
+  const now = dayjs();
+  const columns = getCalendarColumns(now);
+  const ListHeaderComponent = () => {
+    const currentDateText = dayjs(now).format("YYYY.MM.DD.");
+
+    return (
+    <View>
+      {/* < YYYY.MM.DD. > */}
+      <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+        <ArrowButton iconName="arrow-left" onPress={() => {}}/>
+        <TouchableOpacity>
+          <Text style={{ fontSize: 20, color: "#404040"}}>{currentDateText}</Text>
+        </TouchableOpacity>
+        <ArrowButton iconName="arrow-right" onPress={() => {}}/>
+      </View>
+
+      {/* 일 ~ 토 */}
+      <View style={{ flexDirection: "row" }}>
+        {[0, 1, 2, 3, 4, 5, 6].map(day => {
+          const dayText = getDayText(day);
+          const color = getDayColor(day);
+          return (
+            <Column key={`day-${day}`} text={dayText} color={color} opacity={1}/>
+          );
+        })}
+      </View>
+    </View>
+    );
+  };
+  const renderItem = ({ item: date }) => {
+    const dateText = dayjs(date).get("date");
+    const day = dayjs(date).get('day');
+    const color = getDayColor(day);
+    const isCurrentMonth = dayjs(date).isSame(now, 'month');
+    return (
+      <Column text={dateText} color={color} opacity={isCurrentMonth ? 1 : 0.4}/>
+    );
+  };
+
   useEffect(() => {
-    runPracticeDayjs();
+    // runPracticeDayjs();
+    // console.log('columns', columns);
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={columns}
+        keyExtractor={(_, index) => `column${index}`}
+        numColumns={7}
+        renderItem={renderItem}
+        ListHeaderComponent={ListHeaderComponent}
+      />
+    </SafeAreaView>
   );
 }
 
---
+++ todo-calendar/src/Margin.js
@@ -0,0 +1,8 @@
+import { View } from 'react-native';
+import React from 'react';
+
+export default Margin = ({ height }) => {
+  return (
+    <View style={{ height }}/>
+  );
+};
