47c3d0c4c69f9ee165662beef91f2731fe7bd668
Yumin Chu <yuminc03@gmail.com>
2024-04-25 16:44:25 +0900

    Feat: List renderItem에 data.js에서 준비한 데이터 매핑

--- kakao-bus/App.js
+++ kakao-bus/App.js
@@ -1,18 +1,70 @@
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, SectionList, StyleSheet, Text } from 'react-native';
+import dayjs from 'dayjs';
+
 import BusInfo from './src/BusInfo';
-import { COLOR } from './src/color';
+import { busStop, getBusNumColorByType, getSections, getRemainedTimeText, getSeatStatusText } from './src/data';
 
 export default function App() {
+  const sections = getSections(busStop.buses);
+  const now = dayjs();
+  const renderItem = ({ item: bus }) => {
+    const numColor = getBusNumColorByType(bus.type);
+
+    /**
+     * Start
+     */
+    // undefined ?? null -> null 
+    // { ... } ?? null -> { ... }
+    const firstNextBusInfo = bus.nextBusInfos?.[0] ?? null; 
+    const secondNextBusInfo = bus.nextBusInfos?.[1] ?? null;
+    const newNextBusInfos =
+      !firstNextBusInfo && !secondNextBusInfo
+        ? [null]
+        : [firstNextBusInfo, secondNextBusInfo];
+
+    // if (bus.num === 2000) {
+    //   console.log(bus.num, 'newNextBusInfos', newNextBusInfos); // TODO: 확인
+    // }
+
+    const processedNextBusInfos = newNextBusInfos.map((info) => {
+      if (!info)
+        return {
+          hasInfo: false,
+          remainedTimeText: "도착 정보 없음",
+        };
+
+      const { arrivalTime, numOfRemainedStops, numOfPassengers } = info;
+      const remainedTimeText = getRemainedTimeText(now, arrivalTime);
+      const seatStatusText = getSeatStatusText(bus.type, numOfPassengers);
+      return {
+        hasInfo: true,
+        remainedTimeText,
+        numOfRemainedStops,
+        seatStatusText,
+      };
+    });
+    /**
+     * End
+     */
+    return (
+      <BusInfo
+        isBookmarked={bus.isBookmarked}
+        onPressBookmark={() => {}} // TODO
+        num={bus.num}
+        directionDescription={bus.directionDescription}
+        numColor={numColor}
+        processedNextBusInfos={processedNextBusInfos}
+      />
+    );
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <BusInfo
-        isBookmarked={true}
-        onPressBookmark={() => {
-          
-        }}
-        num={146}
-        directionDescription="강남역,강남역사거리"
-        numColor={COLOR.BUS_B}
+      <SectionList
+        style={{ flex: 1, width: '100%' }}
+        sections={sections}
+        renderSectionHeader={({ section: { title }} ) => <Text>{title}</Text>}
+        renderItem={renderItem}
       />
     </SafeAreaView>
   );
--- kakao-bus/src/BusInfo.js
+++ kakao-bus/src/BusInfo.js
@@ -10,27 +10,46 @@ export default ({
   onPressBookmark,
   num,
   directionDescription,
-  numColor
+  numColor,
+  processedNextBusInfos
 }) => {
   return (
     <View style={{ flexDirection: 'row' }}>
-      <View style={{ flex: 1, flexDirection: 'row' }}>
+      <View style={{ 
+        flex: 1, 
+        flexDirection: 'row',
+        alignItems: 'center'
+      }}>
         {/* 북마크 */}
         <BookMarkButton 
           isBookmarked={isBookmarked} 
           onPress={onPressBookmark}
           style={{ paddingHorizontal: 10 }}
         />
+
         {/* 버스번호, 방향 */}
         <View style={{ flex: 1 }}>
           <Text style={{ color: numColor, fontSize: 20 }}>{num}</Text>
           <Text style={{ fontSize: 13, color: COLOR.GRAY_3 }}>{directionDescription} 방향</Text>
         </View>
       </View>
-      <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+
+      <View style={{ 
+        flex: 1, 
+        flexDirection: 'row', 
+        alignItems: 'center'
+      }}>
         {/* M분 S초 / N번째 전 / 여유 */}
         <View style={{ flex: 1 }}>
-          <NextBusInfo
+          {processedNextBusInfos.map(info => (
+            <NextBusInfo
+              hasInfo={info.hasInfo}
+              remainedTimeText={info.remainedTimeText}
+              numOfRemainedStops={info.numOfRemainedStops}
+              seatStatusText={info.seatStatusText}
+            />
+          ))}
+          {/* <NextBusInfo
             hasInfo={true}
             remainedTimeText={'8분 0초'}
             numOfRemainedStops={5}
@@ -39,7 +58,7 @@ export default ({
           <NextBusInfo
             hasInfo={false}
             remainedTimeText='도착 정보 없음'
-          />
+          /> */}
         </View>
         
         {/* 알림아이콘 */}
