fe2a9d1e70db9b5346b0509a0719d471658d4e71
Yumin Chu <yuminc03@gmail.com>
2024-04-28 17:26:27 +0900

    Feat: custom SplashScreen 적용

--- translation/App.js
+++ translation/App.js
@@ -1,12 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { useTranslation } from './src/use-translation';
 import Button from './src/Button';
 import { useCookie } from './src/use-cookie';
+import { useEffect } from 'react';
+
+/**
+ * 스플래시 스크린의 아이콘 출처: https://kor.pngtree.com/freepng/fresh-made-crispy-fortune-cookie_6323404.html
+ * 스플래시 스크린 만들기 가이드: https://docs.expo.dev/guides/splash-screens/
+ * 스플래시 스크린 피그마 템플릿: https://www.figma.com/community/file/1155362909441341285
+ */
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const { t, locale, setLocale } = useTranslation();
   const { cookieKey } = useCookie();
+  
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 2000)
+  }, []);
 
   if (locale === null) return null;
   
