1ee87973f0a54648ac373f99b7c19efa5d0073ff
Yumin Chu <yuminc03@gmail.com>
2024-04-28 17:07:32 +0900

    Feat: AsyncStorage에 선택한 locale 저장, 불러오기

--- translation/src/use-translation.js
+++ translation/src/use-translation.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getLocales } from 'expo-localization';
 import { I18n } from 'i18n-js';
 import { useEffect, useState } from 'react';
@@ -11,12 +12,25 @@ const i18n = new I18n({
   ko, en, ja, zh
 });
 const deviceLanguage = getLocales()[0].languageCode;
+const LOCALE_KEY = "locale";
 
 export const useTranslation = () => {
-  const [locale, setLocale] = useState(null);
+  const [locale, _setLocale] = useState(null);
+  const setLocale = (v) => {
+    _setLocale(v);
+    AsyncStorage.setItem(LOCALE_KEY, v);
+  };
+  const init = async () => {
+    const fs = await AsyncStorage.getItem(LOCALE_KEY);
+    if (fs !== null) {
+      _setLocale(fs);
+    } else {
+      _setLocale(deviceLanguage);
+    }
+  };
 
   useEffect(() => {
-    setLocale(deviceLanguage);
+    init();
   }, []);
 
   return {
